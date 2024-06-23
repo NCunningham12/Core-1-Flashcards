4efa01ef4443a5a5c08d35fc854169f6215af456
Nathan Cunningham <counterweight22@gmail.com>
2024-06-22 19:50:20 -0700

    Game finished wihtout randomization

--- flashcards.py
+++ flashcards.py
@@ -1,58 +1,93 @@
+import random
+
 print("Welcome.")
-begin = input("Type 'OK' to begin")
+begin = input("Want to pass CompTIA Core 1?\n")
 
 cards = [
   {
     "question": "FTP",
-    "answer": "20/21"
+    "answer": "20/21",
+    "correct": False
   },
   {
     "question": "Telnet",
-    "answer": "25"
+    "answer": "23",
+    "correct": False
   },
   {
     "question": "SMTP",
-    "answer": "25"
+    "answer": "25",
+    "correct": False
   },
   {
     "question": "POP3",
-    "answer": "110"
+    "answer": "110",
+    "correct": False
   },
   {
     "question": "IMAP",
-    "answer": "143"
+    "answer": "143",
+    "correct": False
   },
   {
     "question": "DNS",
-    "answer": "53"
+    "answer": "53",
+    "correct": False
   },
   {
     "question": "HTTP",
-    "answer": "80"
+    "answer": "80",
+    "correct": False
   },
   {
     "question": "HTTPS",
-    "answer": "443"
+    "answer": "443",
+    "correct": False
   },
   {
     "question": "RDP",
-    "answer": "3389"
+    "answer": "3389",
+    "correct": False
   },
   {
     "question": "SNMP",
-    "answer": "161"
+    "answer": "161/162",
+    "correct": False
   },
   {
     "question": "DHCP",
-    "answer": "67/68"
+    "answer": "67/68",
+    "correct": False
   },
 ]
 
 def run_flashcards():
-  return
 
+  while True:
+    for card in cards:
+      if card["correct"] == True:
+        continue
+      else:
+        print("\n\n" + card["question"] + ":")
+        user_answer = input("\n")
 
-if begin.upper() == "OK":
+        if user_answer == card["answer"]: 
+          print("\nCORRECT!\n\n")
+          card["correct"] = True
+        else:
+          print("\nIncorrect.\n\n")
+          print("Correct answer: " + card["answer"])
+
+    end = input("Would you like to end the session?\n")
+    if end.lower().startswith("y"):
+      print("Sounds good. Have a good day!")
+      break
+    else:
+      print("Let's try again!")
+      continue
+
+if begin.lower().startswith("y"):
+  print("\nName the following well known ports\n")
   run_flashcards()
 else:
   print("Maybe later then")
